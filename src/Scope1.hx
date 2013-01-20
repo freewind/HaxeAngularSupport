e6fa970e43e87d0a085ffa9e79c494fbcfc429b1
freewind <nowind_lee@qq.com>
2013-01-21 00:22:02 +0800

    Update src/Scope1.hx

--- src/Scope1.hx
+++ src/Scope1.hx
@@ -1,18 +1,34 @@
-package ;
+package xxx;
 
 import haxe.Public;
 
+@:keep
 class Scope1 implements Public, implements freewind.Qkdny {
 
     public function new(scope:Dynamic) {
     }
 
     public var name:String = "myname";
+    public var user:User ;
+    public static var xxname = "xxname";
 
-    public function hello() {
-        var x = name;
-        function() {
-            var y = name;
-        }
+    static function test():String {
+        return "xx";
     }
+
+    private static var sname = "static name";
+
+    public function hello1() {
+         var ss =test();
+    }
+
+    public function hello2() {
+        test();
+    }
+
+
+}
+
+private typedef User = {
+name:String
 }
