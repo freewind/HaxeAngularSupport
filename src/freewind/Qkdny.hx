bbc2d03b587627a479731e43411c4886668f8b9f
freewind <nowind_lee@qq.com>
2013-01-21 01:22:24 +0800

    New arranged code

--- src/freewind/Qkdny.hx
+++ src/freewind/Qkdny.hx
@@ -1,162 +1,180 @@
 package freewind;
 
-import haxe.macro.Expr;
-#if macro
 import haxe.macro.Context;
-import tink.macro.tools.ExprTools;
+import haxe.macro.Expr;
 
+#if macro
+import tink.macro.tools.ExprTools.VarDecl;
+import tink.macro.tools.ExprTools;
 using tink.macro.tools.MacroTools;
 using tink.core.types.Outcome;
-using Lambda;
 #end
+using Lambda;
 
-@:autoBuild(freewind.QkdnyBuilder.build()) interface Qkdny {
+@:autoBuild(freewind.QkdnyBuilder.build())
+interface Qkdny {
 }
 
 class QkdnyBuilder {
 
-    private static var staticFields:Array<haxe.macro.Field>;
-
     @:macro public static function build():Array<haxe.macro.Field> {
-        var fields = Context.getBuildFields();
+        init();
 
-        ctor = getCtor(fields);
-        instanceFieldNames = getInstanceFieldNames(fields);
+        moveInstanceVars();
+        handleCtorBody();
+        moveInstanceFunctions();
+
+        removeFields();
+
+        return allFields;
+    }
+
+    #if macro
+
+ private static var allFields:Array<Field>;
+
+    private static var staticFields:Array<haxe.macro.Field>;
+    private static var instanceNames:Array<String>;
+
+    private static var ctor:Function;
+
+    private static var instanceHolder:Expr;
+
+    private static var ctorOriBlock:Array<Expr>;
+    private static var block:Array<Expr>;
+
+    private static var toRemove:Array<Field>;
+
+    private static function init() {
+        allFields = Context.getBuildFields();
 
         staticFields = [];
-        for (f in fields) {
-            if (f.access.has(AStatic)) staticFields.push(f);
-        }
-
-        var block = getExprs(ctor);
-
-        var blockOriBody = block.copy();
-
-        block.splice(0, block.length);
-
-        var targetObjExpr = getTargetObjName(ctor).resolve();
-
-        var toRemove = new Array<haxe.macro.Field>();
-
-        for (ex in transformBlock(targetObjExpr, blockOriBody, ctor))
-            block.push(ex);
-
-        for (f in fields) {
+        instanceNames = [];
+        for (f in allFields) {
+            if (f.access.has(AStatic)) {
+                staticFields.push(f);
+            } else {
+                instanceNames.push(f.name);
+            }
             switch(f.kind) {
-                case FFun(func):
-                    if (f.name != 'new' && !f.access.has(AStatic)) {
-                        newFunc(block, targetObjExpr, f.name, func);
-                        toRemove.push(f);
+                case FFun(func) :
+                    if (!f.access.has(AStatic) && f.name == 'new') {
+                        ctor = func;
                     }
-                case FVar(t, v):
-                    if (!f.access.has(APrivate) && !f.access.has(AStatic))
-                        newField(block, targetObjExpr, f.name, v == null ? (macro null) : v);
-
-                if (!f.access.has(AStatic))
-                toRemove.push(f);
                 default:
             }
         }
 
-        for (f in toRemove)
-            fields.remove(f);
+        if (ctor == null) throw new Error("No 'function new()' found", Context.currentPos());
+        if (ctor.args.length == 0) throw new Error("Try to use the first argument from ctor, but no args found", Context.currentPos());
 
-        return fields;
-    }
+        instanceHolder = ctor.args[0].name.resolve();
 
-    #if macro
-    static var instanceFieldNames:Array<String>;
-    static var ctor:Function;
-
-    static function getInstanceFieldNames(fields:Array<haxe.macro.Field>):Array<String> {
-        var names = new Array<String>();
-        for (f in fields) {
-              if(!f.access.has(AStatic)) {
-                names.push(f.name);
-              }
+        switch(ctor.expr.expr) {
+            case EBlock(b): block = b;
+            default: block = [ctor.expr];
         }
-        return names;
+
+        ctorOriBlock = block.copy();
+        block.splice(0, block.length);
+        toRemove = [];
     }
 
-    static function scope(target:Expr) {
-        var ctx = [ { name : target.getIdent().sure(), type: macro : Dynamic, expr: null } ];
-        for(f in staticFields) ctx.push({ name:f.name, type:null, expr:f.toExpr() });
-//            switch(f.kind) {
-//                case FFun(func): {
-//                  var args = func.args.map(function(item) return item.type);
-//                  ctx.push({ name: f.name, type: TFunction(args, func.ret), expr: null });
-//                }
-//                case FVar(_,_): ctx.push({ name: f.name, type: macro:Dynamic, expr: null });
-//                default:
-//            }
+
+    private static function moveInstanceVars() {
+        // ignore private ones
+        for (f in allFields) {
+            switch(f.kind) {
+                case FVar(t, v): if (!f.access.has(AStatic) && !f.access.has(APrivate)) {
+                    block.push(newField(f.name, v == null ? (macro null) : v));
+                    toRemove.push(f);
+                }
+                default:
+            }
+        }
+    }
+
+    private static function handleCtorBody() {
+        for(x in ctorOriBlock.mapArray(makeTransformer(ctor.args), defaultScope())) {
+            block.push(x);
+        }
+    }
+
+    private static function moveInstanceFunctions() {
+        for (f in allFields) {
+            switch(f.kind) {
+                case FFun(func): if (!f.access.has(AStatic)) {
+                    if (f.name == 'new') continue;
+                    trace("### processing func: " + f.name);
+                    func.expr = func.expr.map(makeTransformer(func.args), defaultScope());
+                    block.push(
+                        newField(f.name, EFunction(null, func).at())
+                    );
+                    toRemove.push(f);
+                }
+                default:
+            }
+        }
+    }
+
+    private static function removeFields() {
+        for (f in toRemove) {
+            allFields.remove(f);
+        }
+    }
+
+    static function defaultScope() {
+        var ctx = [
+            { name: instanceHolder.getIdent().sure(), type: macro : Dynamic, expr: null }
+        ];
+        for (f in staticFields) {
+            switch(f.kind) {
+                case FFun(func):
+                    ctx.push({ name:f.name, type:null, expr:func.toExpr() });
+                case FVar(t, e):
+                    ctx.push({ name: f.name, type: t, expr: e });
+                default:
+            }
+        }
         return ctx;
     }
 
 
-    static function transformBlock(target:Expr, block:Array<Expr>, ctor:Function):Array<Expr>
-        return block.mapArray(makeTransformer(target, ctor), scope(target))
-
-    static function getExprs(ctor:Function):Array<Expr>
+    private static function newField(varName:String, varValue:Expr) {
         return
-            switch(ctor.expr.expr) {
-                case EBlock(exprs): exprs;
-                default: [ctor.expr];
+            {
+            pos: Context.currentPos(),
+            expr: EBinop(OpAssign, {
+            pos: Context.currentPos(),
+            expr: EField(instanceHolder, varName)
+            }, varValue)
             }
-
-    static function newField(block:Array<Expr>, target:Expr, varName:String, varValue:Expr)
-        block.push(
-            OpAssign.make(
-                target.field(varName),
-                varValue
-            )
-        )
-
-    static function newFunc(block:Array<Expr>, target:Expr, varName:String, func:Function){
-        func.expr = func.expr.map(makeTransformer(target, func), scope(target));
-        newField(block, target, varName, func.toExpr());
     }
 
-    static function has(haystack:Iterable<{ name: String }>, needle:String) {
+    private static function makeTransformer(args:Array<FunctionArg>) {
+        return function(expr:Expr, ctx:Array<VarDecl>):Expr {
+            if (ctx == null) ctx = defaultScope();
+            switch(expr.getIdent()) {
+                case Success(id):
+                    if (id == 'this') return instanceHolder;
+                    if (!hasName(ctx, id) && !hasName(args, id)) {
+                        if (instanceNames.has(id)) {
+                            return instanceHolder.field(id);
+                        }
+                    }
+                    return expr;
+                case Failure(f):
+            }
+            return expr;
+        }
+    }
+
+    static function hasName(haystack:Iterable<{ name:String }>, needle:String) {
         for (straw in haystack)
             if (straw.name == needle)
                 return true;
         return false;
     }
 
-    static function makeTransformer(target:Expr, func:Function)
-        return function(expr:Expr, ctx:Array<VarDecl>):Expr {
-            return
-                switch(expr.getIdent()) {
-                    case Success(id):
-                        return
-                            if (id == 'this')
-                                target;
-                            else if (!has(ctx, id) && !has(func.args, id) && instanceFieldNames.has(id)) {
-                                target.field(id);
-                            }
-                            else {
-                                expr;
-                            }
-                    case Failure(f): expr;
-                }
-        }
-
-    static function getTargetObjName(ctor:Function):String {
-        if (ctor.args.length == 0)
-            ctor.args.push('scope'.toArg(macro : Dynamic));
-        return
-            ctor.args[0].name;
-    }
-
-    static function getCtor(fields:Array<Field>) {
-        for (f in fields)
-            if (f.name == "new")
-                switch(f.kind) {
-                    case FFun(func): return func;
-                    default: throw 'assert';//shouldn't happen
-                }
-
-        return [].toBlock().func(['scope'.toArg(macro : Dynamic)], false);
-    }
 #end
 }
